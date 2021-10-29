2aed20c75e01cba3ba0cafbf6df7b6fc75b328a1
曹磊 <lcaotx@gmail.com>
2021-10-29 16:37:43 +0800

    test view single question

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ViewQuestionsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -18,5 +21,15 @@ class ViewQuestionsTest extends TestCase
         $response = $this->get('/questions');
 
         $response->assertStatus(200);
+    }
+
+    public function test_user_can_view_a_single_question()
+    {
+        $this->withoutExceptionHandling();
+        $question = Question::factory()->create();
+        $test = $this->get('/questions/' . $question->id);
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
