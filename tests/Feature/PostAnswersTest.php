0677f6c369d58cc43458f3332a9032ca0f74a430
曹磊 <lcaotx@gmail.com>
2021-11-01 10:51:47 +0800

    post answer

--- tests/Feature/PostAnswersTest.php
+++ tests/Feature/PostAnswersTest.php
@@ -16,9 +16,9 @@ class PostAnswersTest extends TestCase
      *
      * @return void
      */
-    public function test_user_can_post_an_answer_to_a_question()
+    public function test_user_can_post_an_answer_to_a_published_question()
     {
-        $question = Question::factory()->create();
+        $question = Question::factory()->published()->create();
         $user = User::factory()->create();
 
         $response = $this->post("/questions/{$question->id}/answers", [
@@ -31,6 +31,20 @@ class PostAnswersTest extends TestCase
         $answer = $question->answers()->where('user_id', $user->id)->first();
         $this->assertNotNull($answer);
 
-        $this->assertEquals(1,$question->answers()->count());
+        $this->assertEquals(1, $question->answers()->count());
+    }
+
+    public function test_user_can_not_post_an_answer_to_an_unpublished_question()
+    {
+        $question = Question::factory()->unpublished()->create();
+        $user = User::factory()->create();
+        $response = $this->withExceptionHandling()
+            ->post("questions/{$question->id}/answers", [
+                'user_id' => $user->id,
+                'content' => 'This is an answer'
+            ]);
+        $response->assertStatus(404);
+        $this->assertDatabaseMissing('answers', ['question_id' => $question->id]);
+        $this->assertEquals(0, $question->answers()->count());
     }
 }
